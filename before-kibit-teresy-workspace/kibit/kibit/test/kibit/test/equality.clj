b1a7a6c831cd7cb97ca174f172812782515994fa
Rijnard van Tonder <rvantonder@gmail.com>
2018-11-10 20:16:24 +0000

    add equality

---
+++ before-kibit-teresy-workspace/kibit/kibit/test/kibit/test/equality.clj
@@ -0,0 +1,20 @@
+(ns kibit.test.equality
+  (:require [kibit.check :as kibit])
+  (:use [clojure.test]))
+
+(deftest equality-are
+  (are [expected-alt-form test-form]
+       (= expected-alt-form (:alt (kibit/check-expr test-form)))
+    '(not= x b) '(not (= x b))
+    '(zero? x) '(= 0 x)
+    '(zero? x) '(= x 0)
+    '(zero? x) '(== 0 x)
+    '(zero? x) '(== x 0)
+    '(pos? x) '(< 0 x)
+    '(pos? x) '(> x 0)
+    '(neg? x) '(< x 0)
+    '(neg? x) '(> 0 x)
+    '(true? x) '(= true x)
+    '(false? x) '(= false x)
+    '(nil? x) '(= nil x)
+    '(nil? x) '(= x nil)))
