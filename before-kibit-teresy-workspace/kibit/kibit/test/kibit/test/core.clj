551a85ad15f307ffd1373d8f145b095ccd8011a3
Rijnard van Tonder <rvantonder@gmail.com>
2018-11-10 21:09:34 +0000

    update core

---
+++ before-kibit-teresy-workspace/kibit/kibit/test/kibit/test/core.clj
@@ -0,0 +1,26 @@
+(ns kibit.test.core
+  (:require [kibit.check :as kibit]
+            [kibit.core :as core]
+            [clojure.core.logic.unifier :as unifier]
+            [kibit.rules :as core-rules])
+  (:use [clojure.test]))
+
+(def all-rules  (map unifier/prep core-rules/all-rules))
+
+(deftest simplify-alts
+  (are [expected-alt test-expr]
+       (= expected-alt (core/simplify test-expr all-rules) (:alt (kibit/check-expr test-expr)))
+    [1 2 3]           '(do [1 2 3])
+    []                '(do [])
+    "Hello"           '(do "Hello")
+    '(when test then) '(do (when test then))
+;   :one              '(do :one)     ; KIBIT CRASHES
+;   {:one 1}          '(do {:one 1}) ; KIBIT CRASHES
+))
+
+;; This test confirms when checking will happen and when it won't
+;(deftest simplify-exprs
+;  (are [expected-expr test-expr]
+;       (= expected-expr (:expr (kibit/check-expr test-expr)))
+;    '(do [1 2 3])   '(do [1 2 3])
+;    nil             '(if (> 2 3) :one :two)))
