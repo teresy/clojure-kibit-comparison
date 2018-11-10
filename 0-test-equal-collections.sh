ebe6fb1b4a92654ff1e0a65914fb4b3242b586d5
Rijnard van Tonder <rvantonder@gmail.com>
2018-11-10 20:51:42 +0000

    why is nested op not reducing

--- 0-test-equal-collections.sh
+++ 0-test-equal-collections.sh
@@ -2,12 +2,12 @@
 
 cd before-kibit-kibit-workspace
 git checkout -- .
-time lein kibit --replace kibit 
+time lein kibit --replace kibit &> /dev/null
 cd ..
 
 cd before-kibit-teresy-workspace
 git checkout -- .
-time ~/rooibos-future/main -d . -filter .clj -templates `echo ~/rooibos-future/catalogue/clojure/kibit/{collections,equality}/* | tr ' ' ','`
+time ~/rooibos-future/main -d . -filter .clj -templates `echo ~/rooibos-future/catalogue/clojure/kibit/{collections,equality,arithmetic}/* | tr ' ' ','`
 cd ..
 
 EQUAL=`./diff-teresy-vs-kibit.sh`
